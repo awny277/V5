ecca72978e902b83f78f61a00abeea27893b45aa
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-12-12 16:08:38 +0200

    update

--- JS/submitForm.js
+++ JS/submitForm.js
@@ -1,7 +1,4 @@
-// let submit = document.getElementById("submit");
-// submit.addEventListener("click",
-//     function () {
-//         alert("YOUR MESSAGE HAS BEEN SENT");
-
-//     }
-// )
+let submit = document.getElementById("submit");
+submit.addEventListener("click", function () {
+  alert("YOUR MESSAGE HAS BEEN SENT");
+});
